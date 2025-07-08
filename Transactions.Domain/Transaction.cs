1c4c7da7391089bff6887f8288b7e4c3fd81dbad
Sergey Babin <eld3r@bk.ru>
2025-07-09 02:16:57 +0300

    add Domain

---
+++ Transactions.Domain/Transaction.cs
@@ -0,0 +1,8 @@
+﻿namespace Transactions.Domain;
+
+public record Transaction 
+{ 
+    public required Guid Id { get; init; } 
+    public required DateTime TransactionDate { get; init; } 
+    public required decimal Amount { get; init; } 
+}
